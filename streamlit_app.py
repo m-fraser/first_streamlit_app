36f502ee26697bc5bf663a8687030268ee4e4f9d
m-fraser <103281873+m-fraser@users.noreply.github.com>
2022-04-25 10:37:44 -0400

    Update streamlit_app.py
    Filter table based on picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # adding picklist
-streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 # display the table
 streamlit.dataframe(my_fruit_list)
